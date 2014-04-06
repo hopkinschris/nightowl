8110c81adbc3c3b185d81957669bb9f62cdc105e
Christopher Hopkins <chris@hopkins.io>
2014-04-06 09:32:27 -0400

    run sentiment initialization if in production

--- config/initializers/sentimentalizer_initializer.rb
+++ config/initializers/sentimentalizer_initializer.rb
@@ -2,6 +2,6 @@ require 'sentimentalizer'
 
 NightOwl::Application.configure do
   config.after_initialize do
-    Sentimentalizer.setup
+    Sentimentalizer.setup if Rails.env.production?
   end
 end
